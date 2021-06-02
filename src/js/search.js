c37bbf427db7f12652767f67432a44f5dadcdea8
MichilleZ <michillezhang001@gmail.com>
2021-06-02 12:15:51 -0400

    nj lb

--- src/js/search.js
+++ src/js/search.js
@@ -1,43 +1,42 @@
 import { DOMSelectors } from "./DOM";
 
-const searchInput = document.getElementById(`search-input`);
-let data = [];
-
-console.log(searchInput);
-
-searchInput.addEventListener(`keyup`, (e) => {
-  const searchString = e.target.value;
-  const filteredSongs = data.filter( title => {
-    return title.includes(searchString) || artist.includes(searchString);
-  }); 
-  displayLyrics(filteredSongs);
-});
-
-const query = async function () {
-  try {
-    const response = await fetch(
-      `https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime`
-    );
-    const data = await response.json();
-    displayLyrics(lyrics);
-    console.log(data);
-    
-  } catch (error) {
-    console.log(error);
-    alert("why cant you do anything right");
-  }
-};
-
-query();
-
-const lyrics = document.getElementById("lyrics");
-
-
 const displayLyrics = (lyrics) => {
   const htmlString = lyrics
   lyrics.innerHTML = htmlString;  
   
 }
+const searchInput = document.getElementById(`search-input`);
+let data = [];
+console.log(searchInput);
+
+searchInput.addEventListener(`keyup`, (e) => {
+  const searchString = e.target.value;
+  const query = async function () {
+    try {
+      const response = await fetch(
+        `https://api.lyrics.ovh/v1/Coldplay/`
+      );
+      const data = await response.json();
+      displayLyrics(lyrics);
+      console.log(data);
+      
+    } catch (error) {
+      console.log(error);
+      alert("why cant you do anything right");
+    }
+  };
+  console.log(e.target.value);
+  query();
+});
+
+const filteredSongs = data.filter( title => {
+  return title.includes(searchString);
+}); 
+displayLyrics(filteredSongs);
+
+const lyrics = document.getElementById("lyrics");
+
+
 
 
 
