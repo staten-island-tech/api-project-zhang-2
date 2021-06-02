67a49823bc1965a281377bf2e0ad77207ffdf80f
mr. whalen <michael.whalen@sitechhs.com>
2021-06-02 12:25:26 -0400

    sdfgsdfgsfd

--- src/js/search.js
+++ src/js/search.js
@@ -1,42 +1,35 @@
 import { DOMSelectors } from "./DOM";
 
 const displayLyrics = (lyrics) => {
-  const htmlString = lyrics
-  lyrics.innerHTML = htmlString;  
-  
-}
+  const htmlString = lyrics;
+  lyrics.innerHTML = htmlString;
+};
 const searchInput = document.getElementById(`search-input`);
 let data = [];
 console.log(searchInput);
 
 searchInput.addEventListener(`keyup`, (e) => {
   const searchString = e.target.value;
-  const query = async function () {
+  const query = async function (searchString) {
     try {
       const response = await fetch(
-        `https://api.lyrics.ovh/v1/Coldplay/`
+        `https://api.lyrics.ovh/v1/Coldplay/${searchString}`
       );
       const data = await response.json();
-      displayLyrics(lyrics);
+      displayLyrics(data);
       console.log(data);
-      
     } catch (error) {
       console.log(error);
       alert("why cant you do anything right");
     }
   };
   console.log(e.target.value);
-  query();
+  query(searchString);
 });
 
-const filteredSongs = data.filter( title => {
+const filteredSongs = data.filter((title) => {
   return title.includes(searchString);
-}); 
+});
 displayLyrics(filteredSongs);
 
 const lyrics = document.getElementById("lyrics");
-
-
-
-
-
